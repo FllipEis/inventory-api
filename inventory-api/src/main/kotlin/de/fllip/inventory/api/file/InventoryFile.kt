45fd2f95c57de5f04b334ceb6b6b806ece1c3453
Philipp Eistrach <p.eistrach@gmail.com>
2021-01-20 14:52:56 +0100

    Set loading title default to null

--- inventory-api/src/main/kotlin/de/fllip/inventory/api/file/InventoryFile.kt
+++ inventory-api/src/main/kotlin/de/fllip/inventory/api/file/InventoryFile.kt
@@ -36,7 +36,7 @@ import de.fllip.inventory.api.type.InventoryType
  */
 data class InventoryFile(
     val title: String = "",
-    val loadingTitle: String? = "§cLoading...",
+    val loadingTitle: String? = null,
     val type: InventoryType = InventoryType.NONE,
     val pagination: PaginationInformation = PaginationInformation(),
     val sections: List<AbstractInventorySection> = emptyList()
